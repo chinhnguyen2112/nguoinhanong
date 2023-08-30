6ee057aa59e5e2279831ab0a440e9ab6c15efc62
Hoàng Tuấn Khanh <agola3bie@gmail.com>
2023-08-30 15:15:22 +0700

    lan 4

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -16,7 +16,7 @@ class Ajax extends CI_Controller
         $time = time();
         $page = $this->input->post('page');
         $page = 20 * ($page - 1);
-        $blog = $this->Madmin->get_limit(" index_blog = 1 AND type = 0 AND time_post <= $time", 'blogs', $page, 20);
+        $blog = $this->Madmin->get_limit("type = 0 AND time_post <= $time", 'blogs', $page, 20);
         $html = '';
         if ($blog != null) {
             foreach ($blog as $val) {
@@ -91,5 +91,76 @@ class Ajax extends CI_Controller
         //         echo "lêu lêu";
         //     }
         // }
+    }
+    public function load_more_cate()
+    {
+        $time = time();
+        $page = $this->input->post('page');
+        $name_page = $this->input->post('name_page');
+        $chuyen_muc = $this->input->post('id_chuyenmuc');
+        $page = 18 * ($page - 1);
+        if ($name_page == 'tag') {
+            $list_tag = $this->Madmin->query_sql("SELECT *  FROM tags  WHERE parent = $chuyen_muc ");
+            $where = '  FIND_IN_SET(' . $chuyen_muc . ',tag) ';
+            foreach ($list_tag as $val) {
+                $where .= ' OR FIND_IN_SET(' . $val['id'] . ',tag) ';
+            }
+            $blog_cate_sql = "SELECT blogs.*,category.name as name_cate,category.alias as alias_cate,category.image as image_cate FROM blogs INNER JOIN category ON blogs.chuyenmuc = category.id WHERE  type = 0 AND time_post <= $time AND ($where) ORDER BY blogs.id  DESC LIMIT $page,  18";
+        } else {
+            $blog_cate_sql = "SELECT blogs.*,category.name as name_cate,category.alias as alias_cate,category.image as image_cate FROM blogs INNER JOIN category ON blogs.chuyenmuc = category.id WHERE  type = 0 AND time_post <= $time ORDER BY blogs.id DESC LIMIT $page,  18";
+        }
+        $blog_cate = $this->Madmin->query_sql($blog_cate_sql);
+        $html = '';
+        if ($blog_cate != null) {
+            foreach ($blog_cate as $val) {
+                $cate = chuyen_muc(['id' => $val['chuyenmuc']]);
+                if ($name_page == 'tag') {
+                    $html .= '<div class="this_train">
+                                    <a href="/' . $val['alias'] . '/">
+                                        <img src="/' . $val['image'] . '" alt="' . $val['title'] . '">
+                                        <div class="box_right_data">
+                                            <p class="title_blog">' . $val['title'] . '</p>
+                                            <p class="date_post"><span>' . date('d-m-Y', $val['created_at']) . '</span></p>
+                                            <div class="des_blog">' . $val['sapo'] . '</div>
+                                        </div>
+                                    </a>
+                                </div>';
+                } else {
+                    $html .= '
+                            <div class="this_train">
+                                <a title="' . $val['title'] . '" href="/' . $val['alias'] . '/">
+                                    <p class="title_blog only_mobile">' . $val['title'] . '</p>
+                                </a>
+                                <a href="/' . $val['alias'] . '/">
+                                    <img src="/' . $val['image'] . '" alt="' . $val['title'] . '">
+                                    <div class="box_right_data">
+                                        <p class="title_blog">' . $val['title'] . '</p>
+                                        <div class="fl_date">
+                                                <p class="cate_post">' . $cate[0]['name'] . '</p>
+                                                <span class="dot_item"></span>
+                                                <p class="date_post">' . date('d-m-Y', $val['created_at']) . '</p> 
+                                            </div>
+                                        <div class="des_blog">' . $val['sapo'] . '</div>
+                                    </div>
+                                </a>
+                            </div>
+                            ';
+                }
+            }
+            $next = 0;
+            if (count($blog_cate) == 18) {
+                $next = 1;
+            }
+            $response = [
+                'status' => 1,
+                'html' => $html,
+                'next' => $next
+            ];
+        } else {
+            $response = [
+                'status' => 0,
+            ];
+        }
+        echo json_encode($response);
     }
 }
